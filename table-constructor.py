32b46e39566a47b4e56071696c76c0f4ef1bfc7f
Charles, Will (ELS-OXF) <w.charles@elsevier.com>
2021-03-12 15:53:33 +0000

    Added location loop

--- table-constructor.py
+++ table-constructor.py
@@ -1,7 +1,17 @@
 import pandas as pd
 import sentiment
 
-locations = pd.read_csv('locations.csv')
+radius = '100km'
 
-pos, neg, neutral = sentiment.main(query="Trump", geocode="51.509865,-0.118092,100km", printing=False)
-print(pos, neg, neutral)
+df_loc = pd.read_csv('locations.csv')
+
+# for row in df_loc.rows:
+for index, row in df_loc.iterrows():
+    name = row['LOCATION']
+    lat = row['LAT']
+    lng = row['LONG']
+
+    geocode = str(lat)+','+str(lng)+','+radius    
+
+    pos, neg, neutral = sentiment.main(query="Trump", geocode=geocode, printing=False)
+    print(pos, neg, neutral)
