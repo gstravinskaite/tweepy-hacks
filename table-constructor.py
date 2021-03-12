46363c79b082ebe81e4164048ffae612b843ae01
gstravinskaite <g.stravinskaite@elsevier.com>
2021-03-12 16:55:23 +0100

    Gabby loop

--- table-constructor.py
+++ table-constructor.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import sentiment
 
+coordinates = pd.read_excel("lat_long.xls", index_col=0, header=0)
+cords = coordinates.values.tolist()
+for cord in cords:
+  pos, neg, neutral = sentiment.main(query="Trump", geocode="{},{},100km".format(cord[0],cord[1]), printing=False)
+  print(pos, neg, neutral)
 
-pos, neg, neutral = sentiment.main(query="Trump", geocode="51.509865,-0.118092,100km", printing=False)
-print(pos, neg, neutral)
+
+#	print("Negative tweets percentage: {} %".format(percent_negative)) 
