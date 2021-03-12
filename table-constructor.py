dd54eda5c91faa3e744861f603d71453d0834557
Charles, Will (ELS-OXF) <w.charles@elsevier.com>
2021-03-12 15:47:36 +0000

    Added locations.csv

--- table-constructor.py
+++ table-constructor.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import sentiment
 
+locations = pd.read_csv('locations.csv')
 
 pos, neg, neutral = sentiment.main(query="Trump", geocode="51.509865,-0.118092,100km", printing=False)
 print(pos, neg, neutral)
